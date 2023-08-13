79048dc54dae61b95122adb8dd3e28aac971da2c
Daniel McGill <danm364@yahoo.com>
2023-08-13 19:14:13 -0400

    css changes

--- server/sql/oneTimeScripts/tables/transactionTable.sql
+++ server/sql/oneTimeScripts/tables/transactionTable.sql
@@ -1,12 +1,21 @@
 
-DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS Transactions;
 
-CREATE TABLE Transactions (
+CREATE TABLE transactions (
     transaction_id INT AUTO_INCREMENT PRIMARY KEY  NOT NULL,
     profile_id INT  NOT NULL,
     transactionType VARCHAR(10),
-    ticker VARCHAR(10) NOT NULL,
+    security_id INT  NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     quantity INT  NOT NULL,
-    date DATETIME
+    transaction_date DATETIME,
+    FOREIGN KEY (security_id) REFERENCES security(security_id),
+    FOREIGN KEY (profile_id) REFERENCES profiles(profile_id)
 );
+
+ALTER TABLE security
+DROP CONSTRAINT Transactions_ibfk_1;
+
+SELECT CONSTRAINT_NAME, CONSTRAINT_TYPE
+FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+WHERE TABLE_NAME='Transactions';
