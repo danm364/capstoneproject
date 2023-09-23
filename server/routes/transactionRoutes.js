6d8c966dc9b155d669f9884d85abfb31578de2fb
Daniel McGill <danm364@yahoo.com>
2023-09-23 15:26:15 -0400

    Refactor of transaction Routes Completed

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -2,191 +2,99 @@ const express = require('express');
 const router = express.Router();
 const pool = require("../database/sqlDb");
 
-router.post("/buy", (req, res) => {
+router.post('/buy', async (req, res) => {
+    try {
+      const data = req.body;
+  
+      // Get security ID based on the ticker symbol
+      const secID = await getSecurityId(data.ticker);
 
-    data = req.body
+      console.log(secID)
+  
+      // Get profile cash and total quantity of buys
+      const cashAmount = await getProfileCash(data.profile, secID);
+      const quantityOfBuys = await getQuantityOfBuys(data.profile, secID);
+  
+      // Calculate CashOnHand and buysMinusSellsQuantity
+      const CashOnHand = parseFloat(cashAmount[0].CashOnHand);
+      const buysMinusSellsQuantity = parseInt(quantityOfBuys[0].quantityOfBuys) - parseInt(cashAmount[0].totalQuantity - parseInt(quantityOfBuys[0].quantityOfBuys));
 
-    //get security symbol
+      console.log(CashOnHand)
+      console.log(buysMinusSellsQuantity)
 
-    pool.query("SELECT security_id FROM security WHERE symbol = ?", [data.ticker], (err, result) => {
-        if (err) 
-        {
-            console.log(err)
-        }
-        else {
-            let secID = result[0].security_id
+      // Check if there's enough cash and available quantity to make the buy
+      if ((CashOnHand > (data.quantity * data.price)) && buysMinusSellsQuantity > 0) {
+        const newCashOnHand = CashOnHand - data.price * data.quantity;
+  
+        // Update CashOnHand after the buy is posted
+        await updateProfileCash(data.profile, newCashOnHand);
+  
+        // Insert the buy transaction
+        await insertBuyTransaction(data.profile, secID, data.price, data.quantity, data.date);
+  
+        const result = {
+          ticker: data.ticker,
+          price: data.price,
+        };
+  
+        res.send(result);
+      } else {
+        res.json('Not Enough MV'); // Not enough market value (MV)
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
 
-            values = [[data.profile, data.transactionType, secID, data.price, data.quantity, data.date]]
+  router.post('/sell', async (req, res) => {
+    try {
+      const data = req.body;
+  
+      // Get security ID based on the ticker symbol
+      const secID = await getSecurityId(data.ticker);
+  
+      // Get profile cash, total quantity and total quantity of sells
+      const cashAmount = await getProfileCash(data.profile, secID);
+      const quantityOfSells = await getQuantityOfSells(data.profile, secID);
+      const totalQuantity = cashAmount[0].totalQuantity
+  
+      // Calculate CashOnHand and sellsMinusBuysQuantity
+      let CashOnHand = parseFloat(cashAmount[0].CashOnHand);
+      const sellsMinusBuysQuantity = parseInt(totalQuantity - parseInt(quantityOfSells[0].quantityOfSells)) - parseInt(quantityOfSells[0].quantityOfSells);
+        
+        console.log(cashAmount[0].totalQuantity)
+        console.log(quantityOfSells)
+        console.log(quantityOfSells[0].quantityOfSells)
+        console.log(sellsMinusBuysQuantity)
+        console.log(data.quantity)
+  
+      // Check if there's enough quantity to sell
+      if (sellsMinusBuysQuantity > data.quantity && sellsMinusBuysQuantity > 0) {
+        // Update CashOnHand after the sell is posted
+        CashOnHand += data.price * data.quantity;
+        await updateProfileCash(data.profile, CashOnHand);
+  
+        // Insert the sell transaction
+        await insertSellTransaction(data.profile, secID, data.price, data.quantity, data.date);
+  
+        const result = {
+          ticker: data.ticker,
+          price: data.price,
+        };
+  
+        res.send(result);
+      } else {
+        res.json('Not enough quantity to sell');
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
-            selectProfileCash = "SELECT MAX(P.CashOnHand) CashOnHand, SUM(T.quantity) totalQuantity FROM profiles P INNER JOIN transactions T ON T.profile_id = P.profile_id WHERE T.profile_id = ? AND T.security_id = ? "
-
-                    pool.query(selectProfileCash, [data.profile, secID], (err, cashAmount) => {
-                        if (err) 
-                        {
-                            console.log(err)
-                        }
-                        else {
-
-                            selectBuys = "SELECT SUM(quantity) quantityOfBuys FROM transactions WHERE profile_id = ? AND security_id = ? AND transactionType = 'BUY'"
-
-                            pool.query(selectBuys, [data.profile, secID], (err, quantityOfBuys) => {
-                                if (err) 
-                                {
-                                    console.log(err)
-                                }
-                                else { 
-                                        console.log(cashAmount)
-     
-                                        let CashOnHand = parseFloat(cashAmount[0].CashOnHand)
-                                        
-                                        let buysMinusSellsQuantity = parseInt(quantityOfBuys[0].quantityOfBuys) - parseInt(cashAmount[0].totalQuantity - parseInt(quantityOfBuys[0].quantityOfBuys)) 
-                                        
-
-                                        console.log(CashOnHand)
-                                        console.log((data.quantity * data.price))
-                                        console.log(buysMinusSellsQuantity)
-                                        console.log(data.quantity)
-
-                                        if ((CashOnHand > (data.quantity * data.price)) && (buysMinusSellsQuantity > 0) && (buysMinusSellsQuantity > data.quantity)) {
-
-                                            newCashOnHand = CashOnHand - (data.price * data.quantity)
-
-                                            console.log(newCashOnHand)
-                                            
-                                        //update CashOnHand after buy is posted
-
-                                            updateCash = "UPDATE profiles SET CashOnHand = ? WHERE profile_id = ? "
-
-                                        pool.query(updateCash, [newCashOnHand, data.profile], (err, success) => {
-                                            if (err) 
-                                            {
-                                                console.log(err)
-                                            }
-                                            else {
-
-                                                let buyRequest = 'INSERT INTO stockgamedata.transactions (profile_id, transactionType, security_id, price, quantity, transaction_date) VALUES ?';
-
-                                                pool.query(buyRequest, [values], (err, result) => {
-                                                    if (err) 
-                                                    {
-                                                        console.log(err)
-                                                    }
-                                                    else {
-
-                                                        result = {
-                                                            ticker: data.ticker,
-                                                            price: data.price,
-                                                        }
-                    
-                                                        console.log("1 record inserted")
-                                                        res.send(result)
-
-                                                    }
-
-                                                })
-                                            }
-                                        })
-                                    }
-                                    else {
-                                        res.json("Not Enough MV")
-                                    }
-                                }
-                            })
-                        }
-                    })
-                }
-            })
-        })
-
-router.post("/sell", (req, res) => {
-    data = req.body
-    
-    pool.query("SELECT security_id FROM security WHERE symbol = ?", [data.ticker], (err, result) => {
-
-        if (err) 
-        {
-            console.log(err)
-        }
-        else {
-            
-            let secID = result[0].security_id
-
-            values = [[data.profile, data.transactionType, secID, data.price, data.quantity, data.date]]
-    
-                selectProfileCash = "SELECT MAX(P.CashOnHand) CashOnHand, SUM(T.quantity) totalQuantity FROM profiles P INNER JOIN transactions T ON T.profile_id = P.profile_id WHERE T.profile_id = ? AND T.security_id = ? "
-
-                    pool.query(selectProfileCash, [data.profile, secID], (err, cashAmount) => {
-
-                        if (err) 
-                        {
-                            console.log(err)
-                        }
-                        else {
-
-                            selectSells = "SELECT SUM(quantity) quantityOfSells FROM transactions WHERE profile_id = ? AND security_id = ? AND transactionType = 'SELL'"
-
-                            pool.query(selectSells, [data.profile, secID], (err, quantityOfSells) => {
-                                if (err) 
-                                {
-                                    console.log(err)
-                                }
-                                else { 
-     
-                                    let CashOnHand = parseFloat(cashAmount[0].CashOnHand)
-                                    let buysMinusSellsQuantity = parseInt(cashAmount[0].totalQuantity - parseInt(quantityOfSells[0].quantityOfSells)) - parseInt(quantityOfSells[0].quantityOfSells)
-
-                                    //update value
-                                    if ((buysMinusSells > (data.quantity)) && buysMinusSellsQuantity > 0) {
-
-                                    CashOnHand = CashOnHand + (data.price * data.quantity)
-
-                                    //update CashOnHand after buy is posted
-
-                                    updateCash = "UPDATE profiles SET CashOnHand = ? WHERE profile_id = ? "
-
-                                        pool.query(updateCash, [CashOnHand, data.profile], (err, success) => {
-                                            if (err) 
-                                            {
-                                                console.log(err)
-                                            }
-                                            else {
-
-                                                const sellRequest = 'INSERT INTO stockgamedata.transactions (profile_id, transactionType, security_id, price, quantity, transaction_date) VALUES ?'; 
-            
-                                                pool.query(sellRequest, [values], (err, result) => {
-                                                    if (err) throw err;
-                                                
-                                                    result = {
-                                                        ticker: data.ticker,
-                                                        price: data.price,
-                                                    }
-    
-                                                    console.log("1 record inserted")
-                                                    res.send(result)
-                                                }) 
-                                            }
-                                        }) 
-                                    }
-                                    else {
-                                        res.json("not enough quantity")
-                                    }
-
-                                }
-                            })
-
-
-                            
-                        }
-                    })
-            
-                
-            
-        }
-    })
- 
-})
-
-router.post("/retrieveTrans", (req, res) => {
+  router.post("/retrieveTrans", (req, res) => {
 
     profile_id = req.body.currentAccount
 
@@ -200,5 +108,112 @@ router.post("/retrieveTrans", (req, res) => {
         res.json(result)
     })
 })
+  
+  
+  // Function to get the profile cash and total quantity of sells
+  async function getProfileCash(profile, secID) {
+    return new Promise((resolve, reject) => {
+      const selectProfileCash = `SELECT MAX(P.CashOnHand) CashOnHand, SUM(T.quantity) totalQuantity
+        FROM profiles P INNER JOIN transactions T ON T.profile_id = P.profile_id
+        WHERE T.profile_id = ? AND T.security_id = ?`;
+      pool.query(selectProfileCash, [profile, secID], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+  
+  // Function to get the total quantity of sell transactions
+  async function getQuantityOfSells(profile, secID) {
+    return new Promise((resolve, reject) => {
+      const selectSells = 'SELECT IFNULL(SUM(quantity), 0) quantityOfSells FROM transactions WHERE profile_id = ? AND security_id = ? AND transactionType = "SELL"';
+      pool.query(selectSells, [profile, secID], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+  
+// Function to update the profile's CashOnHand
+    async function updateProfileCash(profile, newCashOnHand) {
+        return new Promise((resolve, reject) => {
+        const updateCash = 'UPDATE profiles SET CashOnHand = ? WHERE profile_id = ?';
+        pool.query(updateCash, [newCashOnHand, profile], (err, result) => {
+            if (err) {
+            reject(err);
+            } else {
+            resolve(result);
+            }
+        });
+        });
+    }
+
+  // Function to update the profile's CashOnHand
+  async function insertSellTransaction(profile, secID, price, quantity, date) {
+    return new Promise((resolve, reject) => {
+      const values = [[profile, 'SELL', secID, price, quantity, date]];
+      const sellRequest = 'INSERT INTO stockgamedata.transactions (profile_id, transactionType, security_id, price, quantity, transaction_date) VALUES ?';
+      pool.query(sellRequest, [values], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log('1 record inserted');
+          resolve(result);
+        }
+      });
+    });
+  }
+
+    // Function to get the security ID based on the ticker symbol
+    async function getSecurityId(ticker) {
+        return new Promise((resolve, reject) => {
+          pool.query('SELECT security_id FROM security WHERE symbol = ?', [ticker], (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result[0].security_id);
+            }
+          });
+        });
+      }
+    
+      
+      // Function to get the total quantity of buy transactions
+      async function getQuantityOfBuys(profile, secID) {
+        return new Promise((resolve, reject) => {
+          const selectBuys = 'SELECT SUM(quantity) quantityOfBuys FROM transactions WHERE profile_id = ? AND security_id = ? AND transactionType = "BUY"';
+          pool.query(selectBuys, [profile, secID], (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          });
+        });
+      }
+      
+      // Function to insert a buy transaction
+      async function insertBuyTransaction(profile, secID, price, quantity, date) {
+        return new Promise((resolve, reject) => {
+          const values = [[profile, 'BUY', secID, price, quantity, date]];
+          const buyRequest = 'INSERT INTO stockgamedata.transactions (profile_id, transactionType, security_id, price, quantity, transaction_date) VALUES ?';
+          pool.query(buyRequest, [values], (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              console.log('1 record inserted');
+              resolve(result);
+            }
+          });
+        });
+      }
+
+
 
 module.exports = router;
